401d1e10b17dba76b4e42f3f01697367bb1c5819
Unknown <davisouzamachado@outlook.com>
2018-05-15 20:54:06 -0300

    Populando a tabela

--- create-table.js
+++ create-table.js
@@ -1,10 +1,10 @@
-const mysql      = require('mysql');
+const mysql      = require('mysql'); // require = módulo = biblioteca JS
 const connection = mysql.createConnection({
   host     : 'localhost',
   port     : 3306,
   user     : 'root',
   password : '',
-  database : ''
+  database : 'nodemysql'
 });
 
 
@@ -15,7 +15,6 @@ connection.connect(function(err){
 })
 
 function createTable(conn){
-
 	const sql = "CREATE TABLE IF NOT EXISTS Clientes (\n"+
 				"ID int NOT NULL AUTO_INCREMENT,\n"+
 				"Nome varchar(150) NOT NULL,\n"+
@@ -26,5 +25,20 @@ function createTable(conn){
 	conn.query(sql, function (error, results, fields){
 		if(error) return console.log(error);
 		console.log("criou a tabela!");
+		addRows(connection);
+	});
+}
+
+function addRows(conn){
+	const sql = "INSERT INTO Clientes(Nome,CPF) VALUES ?";
+	const values = [
+			['teste1', '12345678901'],
+			['teste2', '09876543210'],
+			['teste3', '12312312399']
+		];
+	conn.query(sql, [values], function (error, results, fields){
+		if (error) return console.log(error);
+		console.log('adicionou registros!');
+		conn.end(); // fecha a conexão
 	});
 }
